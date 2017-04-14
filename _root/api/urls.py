386252824447af8923f7854dc387aaa5429ec1d5
Nikos Katsos <nikoskatsos101@gmail.com>
2017-04-14 11:56:04 +0300

    fixed routes under api/

--- _root/api/urls.py
+++ _root/api/urls.py
@@ -3,9 +3,9 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'^students', views.StudentViewSet)
+router.register(r'students', views.StudentViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'', include(router.urls)),
 ]
