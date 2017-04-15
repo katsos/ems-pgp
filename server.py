cc77b35239f20d1de2aab0350c3ff62f66e76fc8
Nikos Katsos <nikoskatsos101@gmail.com>
2017-04-15 05:30:48 +0300

    minor - fixed project structure

--- server.py
+++ server.py
@@ -3,7 +3,12 @@ from flask import Flask
 from server_modules.router import route
 from server_modules.database import Database
 
-app = Flask(__name__, static_url_path='')
+app = Flask(
+    __name__,
+    static_url_path='', # request files inside of static folder without prefixes
+    static_folder='public/static',
+    template_folder='public/templates'
+)
 
 Database.init(app)
 Database.db.create_all()
