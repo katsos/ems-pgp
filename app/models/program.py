23b244bf65eb4c75e9f4884259f3eb1f35b8f90d
Nikos Katsos <nikoskatsos101@gmail.com>
2017-05-07 01:41:18 +0300

    fixed auto increment model value (auto increment value can be only integer)

--- app/models/program.py
+++ app/models/program.py
@@ -2,7 +2,7 @@ from app.database import db
 
 
 class Program(db.Model):
-    id = db.Column(db.String(128), primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Text, nullable=False)
     admin_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
