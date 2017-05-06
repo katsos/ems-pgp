b3edb7277ee7663524980ac02382f52acdcd29c8
Nikos Katsos <nikoskatsos101@gmail.com>
2017-05-06 17:33:37 +0300

    added helper function serializeForm() to serialize each input of a form and construct an object with id(key): value data of each

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,3 +1,14 @@
 String.prototype.capitalize = function () {
   return this[0].toUpperCase() + this.slice(1);
 };
+
+export function serializeForm(node) {
+  const inputs = node.querySelectorAll('input:not([type="submit"])'); // exempt submit input
+
+  const data = {};
+  for (const input of inputs) {
+    Object.assign(data, {[input.id] : input.value});
+  }
+
+  return data;
+}
