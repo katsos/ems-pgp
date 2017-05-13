3ef544287e6091ca171a5a086c9785c767522948
Nikos Katsos <nikoskatsos101@gmail.com>
2017-05-13 15:13:29 +0300

    added  option to server script for quick testing and prototyping

--- server.py
+++ server.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 
 import pymysql
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app
 from app.database import db
+from app.models import *
+
+
+def make_shell_context():
+    return dict(
+        app=app,
+        db=db,
+        GoogleUser=GoogleUser,
+        Program=Program,
+        Student=Student,
+        User=User,
+    )
 
 pymysql.install_as_MySQLdb()
-
 migrate = Migrate(app, db)
+
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == "__main__":
     manager.run()
+
