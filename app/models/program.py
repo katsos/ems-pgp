faf5074d8aa0287527629af85271d9a649e3d270
Nikos Katsos <nikoskatsos101@gmail.com>
2017-05-15 00:34:24 +0300

    fixed Program.title - to be part of constraints need to have a limit (for indexing purposes)

--- app/models/program.py
+++ app/models/program.py
@@ -5,7 +5,7 @@ from app.database import db
 
 class Program(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(256), nullable=False)
     year = db.Column(db.SmallInteger)
 
     creation_date = db.Column(db.DateTime, default=func.now())
