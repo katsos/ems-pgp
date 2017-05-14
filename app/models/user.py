4fafb1f242844859e984fd3322a99be1bfa930d1
Nikos Katsos <nikoskatsos101@gmail.com>
2017-05-15 01:08:58 +0300

    added User.set_google_user

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,7 @@
+from sqlalchemy.exc import InvalidRequestError
+
 from app.database import db
+from app.models.google_user import GoogleUser
 
 
 class User(db.Model):
@@ -10,3 +13,19 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.email
+
+    def set_google_user(self, google_user, token):
+        if self.google_user is None:
+            self.google_user = GoogleUser(email=self.email)
+
+        g = self.google_user
+        g.fullname = google_user['name']
+        g.email_verified = google_user['email_verified']
+        g.image_url = google_user['picture']
+        g.token = token
+
+        try:
+            db.session.commit()
+            return self
+        except InvalidRequestError:
+            return None
