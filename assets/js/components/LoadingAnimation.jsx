00b2d78b8f91fb680814420f3bcbfb362b9a84d1
Nikos Katsos <nikoskatsos101@gmail.com>
2017-05-29 08:32:03 +0300

    implemented LoadingAnimation to use it like `return <LoadingAnimation/>` - doesn’t support fading transition

--- assets/js/components/LoadingAnimation.jsx
+++ assets/js/components/LoadingAnimation.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
-export default function LoadingAnimation(props) {
-  console.log(this.name, props);
-
-  return (
-    <div className="loading-animation-wrapper">
-      <div className={'mdl-spinner mdl-spinner--single-color mdl-js-spinner' + (props.isActive) ? ' is-active' : ''}/>
-    </div>
-  )
-}
+export default LoadingAnimation = () => (
+  <div className="loading-animation-wrapper">
+    <div className={'mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active'}/>
+  </div>
+);
