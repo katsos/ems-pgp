a5690afe8492a28caacbbfd9f6e3f0c6d5e848ff
Nikos Katsos <nikoskatsos101@gmail.com>
2017-06-03 14:25:39 +0300

    used `expose-loader` to expose React object globally now we don't need to `import React from 'react'` in every file.

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,13 @@ module.exports = {
         }
       },
       {
+          test: require.resolve('react'),
+          use: [{
+              loader: 'expose-loader',
+              options: 'React'
+          }]
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
