90b2188ca2601c6bdf7a86b398a5f941a6b86099
Nikos Katsos <nikoskatsos101@gmail.com>
2017-06-04 15:36:35 +0300

    fixed <DrawerUser/> to use window.user to initialize its data

--- assets/js/components/pages/layout/DrawerUser.jsx
+++ assets/js/components/pages/layout/DrawerUser.jsx
@@ -1,10 +1,13 @@
 import LogoutButton from "../../LogoutButton";
 
 export default function DrawerUser(props) {
+  const email = window.user.email;
+  const imgUrl = window.user.google_user.image_url;
+
   return (
     <div className="drawer-user">
-      <img className="drawer-user__photo" src={window.user.picture}/>
-      <span className="drawer-user__email mdl-color-text--grey-600">{window.user.email}</span>
+      <img className="drawer-user__photo" src={imgUrl}/>
+      <span className="drawer-user__email mdl-color-text--grey-600">{email}</span>
       <button className="drawer-user__logout mdl-button mdl-js-button mdl-button--raised">
         <LogoutButton>Logout</LogoutButton>
       </button>
