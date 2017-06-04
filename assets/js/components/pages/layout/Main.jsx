3fbee934448c49bfeaf2f4ee2b6470ddf951e5ab
Nikos Katsos <nikoskatsos101@gmail.com>
2017-06-04 18:44:14 +0300

    refactored `<Main/>` to stateless component

--- assets/js/components/pages/layout/Main.jsx
+++ assets/js/components/pages/layout/Main.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import Dashboard from '../../Dashboard';
 import ProgramsRouter from "../programs/ProgramsRouter";
 
-export default class Main extends React.Component {
-  render() {
-    return (
-      <main className="mdl-layout__content layout__content">
-        <Switch>
-          <Route path="/programs" component={ProgramsRouter}/>
-          <Route component={Dashboard}/>
-        </Switch>
-      </main>
-    )
-  }
+export default function Main() {
+  return (
+    <main className="mdl-layout__content layout__content">
+      <Switch>
+        <Route path="/programs" component={ProgramsRouter}/>
+        <Route component={Dashboard}/>
+      </Switch>
+    </main>
+  )
 }
