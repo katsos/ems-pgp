eba723b8159b2f0dbbca2e27c184b3a93640f45d
Nikos Katsos <nikoskatsos101@gmail.com>
2017-06-06 21:45:45 +0300

    handle loading and failed state of programs fetching

--- assets/js/components/pages/programs/ProgramsList.jsx
+++ assets/js/components/pages/programs/ProgramsList.jsx
@@ -3,6 +3,7 @@ import Link  from 'react-router-dom/Link';
 
 import {ROUTER_PREFIX} from './ProgramsRouter';
 import Http from "../../../Http";
+import LoadingAnimation from "../../LoadingAnimation";
 
 export default class ProgramsList extends React.Component {
   constructor(props) {
@@ -48,13 +49,14 @@ export default class ProgramsList extends React.Component {
 
   render() {
     if (this.state.isLoading) {
-      return <h3>Programs list is loading...</h3>;
-    } else if (this.state.errorEncountered) {
+      return <LoadingAnimation/>;
+    }
+    if (this.state.errorEncountered) {
       return <h3>There was an error while loading programs.</h3>
     }
-
-    if (this.programs.length === 0)
+    if (this.programs.length === 0) {
       return <h3>There are no {this.listState} programs available!</h3>;
+    }
 
     return (
       <div className="programs-list mdl-grid">
