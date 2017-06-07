709ea40cb8d9be1a79f8e7b00abd10cf0ab6a380
Nikos Katsos <nikoskatsos101@gmail.com>
2017-06-07 10:48:28 +0300

    fetch from api program's data in <ProgramPage/>

--- assets/js/components/pages/programs/ProgramPage.jsx
+++ assets/js/components/pages/programs/ProgramPage.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
+import Http from '../../../Http';
+import LoadingAnimation from "../../LoadingAnimation";
+
 export default class ProgramPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.programId = props.match.params.id;
+    this.program = {id: props.match.params.id};
+  }
+
+  componentWillMount() {
+    this.setState({isLoading: true});
+
+    Http.get(`/api/programs/${this.program.id}`)
+      .then(response => response.json())
+      .then(data => {
+        this.program = data;
+        this.setState({isLoading: false});
+      })
+      .catch(response => {
+        alert(response);
+      });
   }
 
   render() {
+    if (this.state.isLoading) return <LoadingAnimation/>;
+
     return (
       <div className="program">
-        Program {this.programId}
+        Program {this.program.title}
       </div>
     )
   }
