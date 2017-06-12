7177d8471850fe36347c57d70f8cf599ab606e2f
Nikos Katsos <nikoskatsos101@gmail.com>
2017-06-12 10:29:02 +0300

    removed unused `String.prototype.camelCaseToSentence()`

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -2,15 +2,6 @@ String.prototype.capitalize = function () {
   return this[0].toUpperCase() + this.slice(1);
 };
 
-String.prototype.camelCaseToSentence = function () {
-  const firstWord = this.match(/^[a-z]*/).pop();
-  let laterWords = this.match(/[A-Z][a-z]*/);
-
-  laterWords = laterWords.map(word => word.toLowerCase());
-
-  return [firstWord.capitalize(), ...laterWords].join(' ');
-};
-
 export function isUndefined(obj) {
   return typeof obj === 'undefined';
 }
