bd630aeb58d39182ef0a7cf0809b96471ba2a56a
Nikos Katsos <nikoskatsos101@gmail.com>
2018-03-23 15:42:54 +0200

    replaced class attr with className

--- assets/js/components/Dashboard.jsx
+++ assets/js/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Dashboard extends React.Component {
   render() {
     return (
-      <div class="Dashboard">
+      <div className="Dashboard">
         This is the dashboard!
       </div>
     )
