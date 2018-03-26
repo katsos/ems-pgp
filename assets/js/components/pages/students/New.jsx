b1a02e0a7c988762d4827fbcf72899d235a3a0b0
Nikos Katsos <nikoskatsos101@gmail.com>
2018-03-26 19:49:41 +0300

    added `/students/new` endpoint on router

---
+++ assets/js/components/pages/students/New.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class New extends React.PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <h3> new student form </h3>
+  }
+}
+
+export default New;
--- assets/js/components/pages/students/Router.jsx
+++ assets/js/components/pages/students/Router.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Index from './Index';
+import New from './New';
 
 function Router() {
   return (
     <Switch>
+      <Route path='/students/new' component={New}/>
       <Route component={Index}/>
     </Switch>
   )
