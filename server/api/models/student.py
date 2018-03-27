70e712373fdd021e9a1a85f4b3305b6be43ad054
Nikos Katsos <nikoskatsos101@gmail.com>
2018-03-27 23:36:23 +0300

    `Student.registered_at` DateTimeField to DateField

--- server/api/models/student.py
+++ server/api/models/student.py
@@ -1,11 +1,11 @@
-from django.db.models import Model, CharField, DateTimeField, EmailField
+from django.db.models import Model, CharField, DateField, DateTimeField, EmailField
 
 
 class Student(Model):
     name = CharField(max_length=256)
     surname = CharField(max_length=256)
     email = EmailField(unique=True)
-    registered_at = DateTimeField()
+    registered_at = DateField()
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
 
