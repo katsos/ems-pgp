2b9cdb28980fd9b8a535a55bd0369c7901f7f47f
Nikos Katsos <nikoskatsos101@gmail.com>
2018-03-29 00:44:42 +0300

    added StudentPage component

--- assets/js/components/pages/students/Router.jsx
+++ assets/js/components/pages/students/Router.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Index from './Index';
+import StudentPage from './StudentPage';
 import New from './New';
+
+const ROUTER_PREFIX = '/students';
 
 function Router() {
   return (
     <Switch>
-      <Route path='/students/new' component={New}/>
+      <Route path={`${ROUTER_PREFIX}/new`} component={New}/>
+      <Route exact path={`${ROUTER_PREFIX}/:id`} component={StudentPage}/>
       <Route component={Index}/>
     </Switch>
   )
---
+++ assets/js/components/pages/students/StudentPage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import LoadingAnimation from "../../LoadingAnimation";
+import Student from "../../../models/Program";
+
+class StudentPage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      student: null,
+    }
+  }
+
+  componentWillMount() {
+    const { student = null } = this.props.location.state;
+    this.setState({ student })
+  }
+  componentDidMount() {
+    if (this.state.student !== null) {
+      return this.setState({ isLoading: false });
+    }
+
+    const { id } = this.props.match.params;
+    Student.get(id)
+      .then(student => this.setState({ student }))
+      .catch(console.error)
+      .finally(() => this.setState({ isLoading: false }));
+  }
+
+  render() {
+    if (this.state.isLoading) return <LoadingAnimation />;
+
+    if (this.state.student === null) return <h3>There was a problem while fetching student data.</h3>;
+
+    return (
+      <div>
+        {this.state.student.id}
+      </div>
+    )
+  }
+}
+
+export default StudentPage;
