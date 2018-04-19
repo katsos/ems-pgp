a4d7017875f449ac2ed68c6f737109789735dd2b
Nikos Katsos <nikoskatsos101@gmail.com>
2018-04-19 13:06:19 +0300

    populated /payments page

--- assets/js/components/pages/payments/Index.jsx
+++ assets/js/components/pages/payments/Index.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import LoadingAnimation from '../../LoadingAnimation';
+import Payment from '../../../models/Payment';
 
 class Index extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      payments: null,
+    };
+  }
+
+  componentDidMount() {
+    Payment.getAll()
+      .then(payments => this.setState({ payments }))
+      .finally(() => this.setState({ isLoading: false }));
+  }
+
   render() {
+    const { isLoading, payments } = this.state;
+
+    if (isLoading) return <LoadingAnimation />;
+
+    if (payments === null) return <h3>There was a problem while fetching payments.</h3>;
+
+    if (payments.length === 0) return <h3>No payments have been done.</h3>;
+
     return (
-      <h3>hello darkness my old friend!</h3>
+      <div>
+        <ul>
+          {payments.map(({ id, amount }) => (
+            <li key={id}>{amount}</li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
