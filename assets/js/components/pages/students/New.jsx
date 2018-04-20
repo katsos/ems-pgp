d5b5c7e3cc86bc3a8a1bb3b5b00e089370c1fe78
Nikos Katsos <nikoskatsos101@gmail.com>
2018-04-20 14:42:29 +0300

    refactored student form without using the Form component

--- assets/js/components/pages/students/New.jsx
+++ assets/js/components/pages/students/New.jsx
@@ -1,46 +1,63 @@
-import React from 'react'
-import Form from '../../Form';
+import React from 'react';
+import moment from 'moment';
 import Student from "../../../models/Student";
-
-const FORM_SCHEMA = {
-  name: {
-    label: 'Name',
-  },
-  surname: {
-    label: 'Surname',
-  },
-  email: {
-    label: 'E-mail',
-    type: 'email',
-  },
-  registered_at: {
-    label: 'Registered at',
-    type: 'date',
-  },
-};
 
 class New extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      formErrors: {},
+      name: '',
+      surname: '',
+      email: 'example@domain.gr',
+      registered_at: moment().format('YYYY-MM-DD'),
+      errors: {},
     };
+    this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onSubmit(params) {
-    Student.create(params)
+  onChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    Student.create(this.state)
       .then(student => this.props.history.push(`/students/${student.id}`, { student }))
-      .catch(({ response: { data: formErrors }}) => {
-        this.setState({ formErrors });
-      });
+      .catch(({ response: { data: errors }}) => this.setState({ errors }));
   }
 
   render() {
+    const { name, surname, email, registered_at, errors } = this.state;
+
     return (
       <div>
-        <Form schema={FORM_SCHEMA} errors={this.state.formErrors} onSubmit={this.onSubmit} />
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Name:</label>
+            <input name='name' value={name} onChange={this.onChange} />
+            {(errors.name || []).map((e) => <div key={e}>{e}</div>)}
+          </div>
+          <div>
+            <label>Surname:</label>
+            <input name='surname' value={surname} onChange={this.onChange} />
+            {(errors.surname || []).map((e) => <div key={e}>{e}</div>)}
+          </div>
+          <div>
+            <label>E-mail:</label>
+            <input type='email' name='email' value={email} onChange={this.onChange} />
+            {(errors.email || []).map((e) => <div key={e}>{e}</div>)}
+          </div>
+          <div>
+            <label>Registered at:</label>
+            <input type='date' name='registered_at' value={registered_at} onChange={this.onChange} />
+            {(errors.registered_at || []).map((e) => <div key={e}>{e}</div>)}
+          </div>
+
+          {/*<button onClick={this.onCancel}>Cancel</button>*/}
+          <input type='submit' value='Submit' />
+        </form>
       </div>
     );
   }
