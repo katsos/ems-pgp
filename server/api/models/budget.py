19289bb4d168ef5dda3b2730062ee41c46e8265b
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-11 16:01:19 +0300

    pylint

--- server/api/models/budget.py
+++ server/api/models/budget.py
@@ -1,6 +1,7 @@
 from django.db.models import Model, DateTimeField, ForeignKey, CASCADE
 from .budget_field import BudgetField
 
+
 class Budget(Model):
     circle = ForeignKey('Circle', on_delete=CASCADE)
     created_at = DateTimeField(auto_now_add=True)
