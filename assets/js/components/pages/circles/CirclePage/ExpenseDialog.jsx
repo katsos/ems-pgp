2fdc768179764674cf46add9b5bea40330c559f3
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-14 23:01:01 +0300

    cosmetic changes and optimizations

--- assets/js/components/pages/circles/CirclePage/ExpenseDialog.jsx
+++ assets/js/components/pages/circles/CirclePage/ExpenseDialog.jsx
@@ -1,59 +1,65 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { Button, Dialog, DialogTitle, withStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import {
+  Button, Dialog, DialogTitle, TextField,
+  FormControl, Input, InputLabel, InputAdornment
+} from '@material-ui/core';
 import { Expense } from '../../../../models';
+import './ExpenseDialog.scss';
 
 class ExpenseDialog extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    const { field } = this.props;
-    this.cycleId = this.props.match.params.id;
     this.state = {
-      budget_field: (field) ? field.id : '',
       type: '',
-      amount: 0,
+      amount: '',
       notes: '',
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange({ target: { name, value }}) {
+  onChange({ target: { name, value } }) {
     this.setState({ [name]: value });
   }
 
   onSubmit() {
-    Expense.create(this.state)
+    const { id: budget_field } = this.props.field;
+    Expense.create({ ...this.state, budget_field })
       // TODO: inform for the successful POST
       .then(this.props.onCancel);
       // TODO: catch
   }
 
   render() {
-    const { classes, field, isOpen, onCancel } = this.props;
-    const { budget_field, type, amount, notes } = this.state;
+    const { isOpen, field, onCancel } = this.props;
+    const { type, amount, notes } = this.state;
 
     // TODO: show form errors
     return (
-      <Dialog open={isOpen}>
-        <DialogTitle>Νέo έξοδο</DialogTitle>
-        <form>
-          <div>
-            <label>Κατηγορία προϋπολογισμού:</label>
-            {field ?
-              <span>{`${field.code} ${field.title}`}</span>
-              : <input name='budget_field' value={budget_field} type='number' />
-            }
-          </div>
-          <div>
-            <label>Περιγραφή:</label>
-            <input name='type' value={type} onChange={this.onChange} />
-          </div>
-          <div>
-            <label>Ποσό:</label>
-            <input name='amount' value={amount} onChange={this.onChange} />
-          </div>
+      <Dialog open={isOpen} className='ExpenseDialog'>
+        <DialogTitle className='ExpenseDialog__title'>
+          Νέo έξοδο
+          <p>{`${field.code} ${field.title}`}</p>
+        </DialogTitle>
+        <form className='ExpenseDialog__form'>
+          <TextField name='type' value={type} onChange={this.onChange} label='Περιγραφή' />
+
+          <FormControl>
+            <InputLabel htmlFor='amount'>Ποσό</InputLabel>
+            <Input
+              id='amount'
+              type='number'
+              name='amount'
+              value={amount}
+              onChange={this.onChange}
+              className='ExpenseDialog__form__amount'
+              endAdornment={<InputAdornment position='end'>&euro;</InputAdornment>}
+            />
+          </FormControl>
+
+
           {/* TODO */}
           {/*<FormControlLabel*/}
             {/*control={*/}
@@ -65,18 +71,33 @@ class ExpenseDialog extends React.PureComponent {
             {/*}*/}
             {/*label='ΟΛΟΚΛΗΡΩΜΕΝΗ'*/}
           {/*/>*/}
-          <div>
-            <label>Σχόλια:</label>
-            <input name='notes' value={notes} onChange={this.onChange} />
+
+          <TextField
+            name='notes'
+            value={notes}
+            onChange={this.onChange}
+            label='Σχόλια'
+            multiline
+            rowsMax='3'
+          />
+
+          <div className='ExpenseDialog__form__buttons'>
+            <Button color='secondary' onClick={onCancel}>ΑΚΥΡΩΣΗ</Button>
+            <Button color='primary' onClick={this.onSubmit}>ΥΠΟΒΟΛΗ</Button>
           </div>
-          <Button color='secondary' className={classes.button} onClick={onCancel}>ΑΚΥΡΩΣΗ</Button>
-          <Button color='primary' className={classes.button} onClick={this.onSubmit}>ΥΠΟΒΟΛΗ</Button>
         </form>
       </Dialog>
     );
   }
 }
 
-const ExpenseDialogWithStyles = withStyles()(ExpenseDialog);
-const ExpenseDialogWithRouter = withRouter(ExpenseDialogWithStyles);
-export default ExpenseDialogWithRouter;
+
+ExpenseDialog.propTypes = {
+  field: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
+
+export default ExpenseDialog;
