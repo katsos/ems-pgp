e688b797f2bb1cc5d9ce4d673fea152eaffef664
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-14 19:19:10 +0300

    fixed: zero payments case

--- assets/js/components/pages/payments/List.jsx
+++ assets/js/components/pages/payments/List.jsx
@@ -37,8 +37,6 @@ class List extends React.PureComponent {
 
     if (payments === null) return <h3>There was a problem while fetching payments.</h3>;
 
-    if (payments.length === 0) return <h3>No payments have been done.</h3>;
-
     return (
       <div className='PaymentList'>
         <div className='PaymentList__header'>
@@ -47,30 +45,36 @@ class List extends React.PureComponent {
             <Button variant='fab' color='primary' aria-label='add'><AddIcon /></Button>
           </Link>
         </div>
-        <Table className='PaymentList__table'>
-          <TableHead>
-            <TableRow>
-              <TableCell className='PaymentList__table__id'>ΑΜΣ</TableCell>
-              <TableCell>ΟΝΟΜΑΤΕΠΩΝΥΜΟ</TableCell>
-              <TableCell numeric>ΚΥΚΛΟΣ</TableCell>
-              <TableCell numeric>ΠΟΣΟ</TableCell>
-              <TableCell numeric>ΗΜΕΡΟΜΗΝΙΑ ΚΑΤΑΧΩΡΗΣΗΣ</TableCell>
-              <TableCell />
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {payments.map(p => (
-              <TableRow key={p.id}>
-                <TableCell className='PaymentList__table__id'>{p.id}</TableCell>
-                <TableCell>{`${p.student.surname} ${p.student.name}`}</TableCell>
-                <TableCell numeric>{p.student.circle}</TableCell>
-                <TableCell numeric>{p.amount}</TableCell>
-                <TableCell numeric>{moment(p.created_at).format('L')}</TableCell>
-                <TableCell><ListActionsButton payment={p} afterAction={this.fetchPayments} /></TableCell>
+        {payments.length === 0 ? (
+          <p>Δεν έχει καταχωρηθει καμία πληρωμή εώς τώρα.<br />
+            Πατήστε το κουμπί δεξιά για να εισάγετε νέα.
+          </p>
+        ) : (
+          <Table className='PaymentList__table'>
+            <TableHead>
+              <TableRow>
+                <TableCell className='PaymentList__table__id'>ΑΜΣ</TableCell>
+                <TableCell>ΟΝΟΜΑΤΕΠΩΝΥΜΟ</TableCell>
+                <TableCell numeric>ΚΥΚΛΟΣ</TableCell>
+                <TableCell numeric>ΠΟΣΟ</TableCell>
+                <TableCell numeric>ΗΜΕΡΟΜΗΝΙΑ ΚΑΤΑΧΩΡΗΣΗΣ</TableCell>
+                <TableCell />
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {payments.map(p => (
+                <TableRow key={p.id}>
+                  <TableCell className='PaymentList__table__id'>{p.id}</TableCell>
+                  <TableCell>{`${p.student.surname} ${p.student.name}`}</TableCell>
+                  <TableCell numeric>{p.student.circle}</TableCell>
+                  <TableCell numeric>{p.amount}</TableCell>
+                  <TableCell numeric>{moment(p.created_at).format('L')}</TableCell>
+                  <TableCell><ListActionsButton payment={p} afterAction={this.fetchPayments} /></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </div>
     );
   }
