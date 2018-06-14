67384b9ca2430faa87e19c0ff319474c2dd84fa9
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-14 15:29:06 +0300

    added related_name

--- server/api/models/payment.py
+++ server/api/models/payment.py
@@ -2,7 +2,7 @@ from django.db.models import Model, CharField, DateTimeField, DecimalField, Fore
 
 
 class Payment(Model):
-    student = ForeignKey('Student', on_delete=CASCADE)
+    student = ForeignKey('Student', related_name='payments', on_delete=CASCADE)
     amount = DecimalField(max_digits=6, decimal_places=2)
     notes = CharField(max_length=256, null=True)
     created_at = DateTimeField(auto_now_add=True)
