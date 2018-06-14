6217ce5d2a47c03565f5240e73c36d51d0ef3476
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-14 12:56:55 +0300

    eslint

--- assets/js/components/pages/layout/Drawer.jsx
+++ assets/js/components/pages/layout/Drawer.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Link from 'react-router-dom/Link';
 import ROUTES from '../../../routes';
 
-export default function Drawer(props) {
+function Drawer({ toggleDrawer }) {
   return (
-    <div className="mdl-layout__drawer">
-      <nav className="mdl-navigation">
-        {NavLinks(props.toggleDrawer)}
+    <div className='mdl-layout__drawer'>
+      <nav className='mdl-navigation'>
+        {ROUTES.map(({ name, url }) => (
+          <Link
+            className='mdl-navigation__link'
+            to={url}
+            key={name}
+            onClick={toggleDrawer}
+          >
+            {name}
+          </Link>
+         ))}
       </nav>
     </div>
   );
 }
 
-function NavLinks(toggleDrawer) {
-  return ROUTES.map(({ name, url }) => {
-     return (
-       <Link
-         className="mdl-navigation__link"
-         to={url}
-         key={name}
-         onClick={toggleDrawer}
-       >{name}</Link>
-     );
-  });
-}
+Drawer.propTypes = {
+  toggleDrawer: PropTypes.func.isRequired,
+};
+
+export default Drawer;
