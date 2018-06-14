0be48020a816a30b7e5472f6a8705eac54a61750
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-14 11:45:30 +0300

    eslint fixes

--- assets/js/components/pages/circles/CirclePage/BudgetSection.jsx
+++ assets/js/components/pages/circles/CirclePage/BudgetSection.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import BudgetFieldRow from './BudgetFieldRow';
 
 function BudgetSection({ budget, circleId }) {
-  if (!budget) return (
-    <div className='CirclePage'>
-      <hr />
-      <p>Δεν έχει καταχωρηθέι προϋπολογισμός για αυτό το πρόγραμμα. <br />
+  if (!budget) {
+    return (
+      <div className='CirclePage'>
+        <hr />
+        <p>Δεν έχει καταχωρηθέι προϋπολογισμός για αυτό το πρόγραμμα. <br />
         Δημιουργήστε τον προϋπολογισμό πατώντας το κουμπί. <br />
-        <span>Δεν θα μπορείτε να καταγράψετε συναλλαγές έως ώτου υπάρξει επιτυχής καταχώρηση.</span>
-      </p>
-      <Link
-        className='mdl-button mdl-js-button mdl-button--raised mdl-button--colored'
-        to={`/circles/${this.circleId}/budget`}
-      >ΔΗΜΙΟΥΡΓΙΑ ΠΡΟΫΠΟΛΟΓΙΣΜΟΥ</Link>
-    </div>
-  );
+          <span>Δεν θα μπορείτε να καταγράψετε συναλλαγές έως ώτου υπάρξει επιτυχής καταχώρηση.</span>
+        </p>
+        <Link
+          className='mdl-button mdl-js-button mdl-button--raised mdl-button--colored'
+          to={`/circles/${circleId}/budget`}
+        >ΔΗΜΙΟΥΡΓΙΑ ΠΡΟΫΠΟΛΟΓΙΣΜΟΥ
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div className='CirclePage__budget'>
@@ -25,20 +29,20 @@ function BudgetSection({ budget, circleId }) {
         <h3>Προϋπολογισμός</h3>
         <Link
           className='mdl-button mdl-js-button mdl-button--primary CirclePage__budget__header__edit'
-          to={{ pathname: `/circles/${circleId}/budget`, state: { budget }, }}
+          to={{ pathname: `/circles/${circleId}/budget`, state: { budget } }}
         >
-          <i className="material-icons">mode_edit</i>
+          <i className='material-icons'>mode_edit</i>
         </Link>
       </div>
       <div>Δημιουργήθηκε στις {moment(budget.created_at).format('L')}</div>
 
       <table className='CirclePage__budget__table'>
         <thead>
-        <tr>
-          <th colSpan='2'>Κατηγορίες Δαπανών</th>
-          <th>Προυπολογυσμός</th>
-          <th />
-        </tr>
+          <tr>
+            <th colSpan='2'>Κατηγορίες Δαπανών</th>
+            <th>Προυπολογυσμός</th>
+            <th />
+          </tr>
         </thead>
         <tbody>
           {budget.fields.map(f => <BudgetFieldRow key={f.id} field={f} />)}
@@ -47,5 +51,14 @@ function BudgetSection({ budget, circleId }) {
     </div>
   );
 }
+
+BudgetSection.propTypes = {
+  circleId: PropTypes.number.isRequired,
+  budget: PropTypes.shape({
+    fields: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
+};
 
 export default BudgetSection;
