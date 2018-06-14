a347f187613d6f6cb5ad7574e3fd84ac1bc32ce8
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-14 12:41:55 +0300

    eslint

--- assets/js/components/pages/layout/Main.jsx
+++ assets/js/components/pages/layout/Main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ROUTES from '../../../routes';
-import Dashboard from "../../Dashboard";
+import Dashboard from '../../Dashboard';
 
 export default function Main() {
   return (
-    <main className="mdl-layout__content layout__content">
+    <main className='mdl-layout__content layout__content'>
       <Switch>
         {ROUTES
           .filter(r => r.name !== 'home')
-          .map(({ name, url, component }) => <Route key={name} path={url} component={component}/>)
+          .map(({ name, url, component }) => <Route key={name} path={url} component={component} />)
         }
-        <Route component={Dashboard}/>
+        <Route component={Dashboard} />
       </Switch>
     </main>
   );
