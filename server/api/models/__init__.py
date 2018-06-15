0f8e7e1aeb1742dead062df64df22dd486f86139
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-16 02:57:02 +0300

    deleted Program model

--- server/api/models/__init__.py
+++ server/api/models/__init__.py
@@ -3,5 +3,4 @@ from .budget_field import BudgetField
 from .circle import Circle
 from .expense import Expense
 from .payment import Payment
-from .program import Program
 from .student import Student
--- server/api/views/students_viewset.py
+++ server/api/views/students_viewset.py
@@ -9,15 +9,6 @@ class StudentsViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentsSerializer
 
-    def get_queryset(self):
-        students = Student.objects.all()
-
-        program_id = self.request.query_params.get('program_id')
-        if program_id is not None:
-            students = students.filter(program__id=program_id)
-
-        return students
-
     @action(methods=['post'], detail=True)
     def set_payment(self, request, pk=None):
         student = self.get_object()
