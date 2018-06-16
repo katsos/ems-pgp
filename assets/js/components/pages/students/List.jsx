161b019dae12fab2764d239f43bfd11138cbb053
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-16 13:32:47 +0300

    fixed the list of students - cosmetic changes and missing funcionality

--- assets/js/components/pages/students/List.jsx
+++ assets/js/components/pages/students/List.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import moment from 'moment';
+import sumBy from 'lodash/sumBy';
+import Link from 'react-router-dom/Link';
+import AddIcon from '@material-ui/icons/Add';
+import { Button, Table, TableHead, TableCell, TableRow, TableBody } from '@material-ui/core';
 import Student from '../../../models/Student';
-import LoadingAnimation from "../../LoadingAnimation";
+import LoadingAnimation from '../../LoadingAnimation';
+import './StudentList.scss';
 
 class Index extends React.PureComponent {
   constructor() {
@@ -20,23 +25,59 @@ class Index extends React.PureComponent {
   }
 
   render() {
-    if (this.state.isLoading) return <LoadingAnimation />;
-
-    if (this.state.students === null) return <h3>There was a problem while fetching students.</h3>;
+    const { isLoading, students } = this.state;
+    if (isLoading) return <LoadingAnimation />;
+    if (students === null) {
+      return (
+        <h3>
+          Υπήρξε ένα πρόβλημα. Παρακαλώ δοκιμάστε να κάνετε ανανέωση τη σελίδα.<br />
+          Εάν το πρόβλημα συνεχίσει να υπάρχει, επικοινωνήστε με τον υπεύθυνο λογισμικού του πανεπιστημίου.
+        </h3>
+      );
+    }
 
     return (
-      <div className='Students'>
-        <ul>
-          {this.state.students.map(({ id, name, surname }) => (
-            <li key={id}><Link to={`/students/${id}`}>{surname} {name}</Link></li>
-          ))}
-        </ul>
-
-        <Link to='/students/new'>
-          <button className='Students__add mdl-button mdl-js-button mdl-button--fab mdl-button--colored'>
-            <i className="material-icons">add</i>
-          </button>
-        </Link>
+      <div className='StudentList'>
+        <div className='StudentList__header'>
+          <h3>Φοιτητές</h3>
+          <Link to='/students/new'>
+            <Button variant='fab' color='primary' aria-label='add'><AddIcon /></Button>
+          </Link>
+        </div>
+        {students.length === 0 ? (
+          <p>Δεν υπάρχει κανένας καταχωρημένος φοιτητής εώς τώρα.<br />
+            Πατήστε το κουμπί στα δεξιά για να εισάγετε νέο.
+          </p>
+        ) : (
+          <Table className='StudentList__table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>ΟΝΟΜΑΤΕΠΩΝΥΜΟ</TableCell>
+                <TableCell>ΚΥΚΛΟΣ</TableCell>
+                <TableCell numeric>ΑΡΙΘΜΟΣ ΠΛΗΡΩΜΩΝ</TableCell>
+                <TableCell numeric>ΣΥΝΟΛΟ ΠΛΗΡΩΜΩΝ</TableCell>
+                <TableCell numeric>ΥΠΟΛΟΙΠΟ</TableCell>
+                <TableCell numeric>ΗΜΕΡΟΜΗΝΙΑ ΚΑΤΑΧΩΡΗΣΗΣ</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {students.map(s => (
+                <TableRow key={s.id}>
+                  <TableCell>
+                    <Link to={`/students/${s.id}`}>{`${s.surname} ${s.name}`}</Link>
+                  </TableCell>
+                  <TableCell>
+                    <Link to={`/circles/${s.circle.id}`}>{s.circle.title}</Link>
+                  </TableCell>
+                  <TableCell numeric>{s.payments.length}</TableCell>
+                  <TableCell numeric>{sumBy(s.payments, 'amount')}</TableCell>
+                  <TableCell numeric>{s.circle.tuition - sumBy(s.payments, 'amount')}</TableCell>
+                  <TableCell numeric>{moment(s.created_at).format('L')}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </div>
     );
   }
--- server/api/serializers/students.py
+++ server/api/serializers/students.py
@@ -1,9 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 from server.api.models import Student
 from .payments_shallow import PaymentSerializerShallow
+from .circles_shallow import CirclesSerializerShallow
 
 
 class StudentsSerializer(ModelSerializer):
+    circle = CirclesSerializerShallow(read_only=True)
     payments = PaymentSerializerShallow(read_only=True, many=True)
 
     class Meta:
