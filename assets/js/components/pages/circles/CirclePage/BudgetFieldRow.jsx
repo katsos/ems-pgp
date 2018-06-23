afe2c7f6329579674661694b982c3212a906d774
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-23 21:52:30 +0300

    eslint

--- assets/js/components/pages/circles/CirclePage/BudgetFieldRow.jsx
+++ assets/js/components/pages/circles/CirclePage/BudgetFieldRow.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
