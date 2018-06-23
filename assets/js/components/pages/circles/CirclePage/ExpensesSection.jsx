1547f01125bb1f96b676d40c954d89b9ed511af1
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-23 21:53:44 +0300

    refactor: pure component to stateless

--- assets/js/components/pages/circles/CirclePage/ExpensesSection.jsx
+++ assets/js/components/pages/circles/CirclePage/ExpensesSection.jsx
@@ -8,46 +8,45 @@ import TableCell from '@material-ui/core/TableCell';
 import CheckMark from '@material-ui/icons/Check';
 import './ExpensesSection.scss';
 
-class ExpensesSection extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
+function ExpensesSection({ budget }) {
+  const fields = budget.fields || [];
+  const expenses = fields
+    .reduce((arr, field) => [...arr, ...field.expenses], [])
+    .sort((a, b) => Date(a.created_at) > Date(b.created_at));
 
-  render() {
-    const expenses = this.props.budget.fields
-      .reduce((arr, field) => [...arr, ...field.expenses], [])
-      .sort((a, b) => Date(a.created_at) > Date(b.created_at));
+  if (!expenses.length) return null;
 
-    return (
-      <div className='ExpensesSection'>
-        <h4>Έξοδα κύκλου</h4>
-        <Table className='ExpensesSection__table'>
-          <TableHead>
+  return (
+    <div className='ExpensesSection'>
+      <h4>Έξοδα κύκλου</h4>
+      <Table className='ExpensesSection__table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>ΑΙΤΙΟΛΟΓΙΑ</TableCell>
+            <TableCell numeric>ΠΟΣΟ</TableCell>
+            <TableCell className='ExpensesSection__table__completed'>
+              ΟΛΟΚΛΗΡΩΜΕΝΟ
+            </TableCell>
+            <TableCell>ΠΑΡΑΤΗΡΗΣΕΙΣ</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {expenses.map(e => (
             <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>ΑΙΤΙΟΛΟΓΙΑ</TableCell>
-              <TableCell numeric>ΠΟΣΟ</TableCell>
+              <TableCell>{e.id}</TableCell>
+              <TableCell>{e.type}</TableCell>
+              <TableCell numeric>{e.amount}</TableCell>
               <TableCell className='ExpensesSection__table__completed'>
-                ΟΛΟΚΛΗΡΩΜΕΝΟ
+                {e.is_completed ? <CheckMark /> : null}
               </TableCell>
-              <TableCell>ΠΑΡΑΤΗΡΗΣΕΙΣ</TableCell>
+              <TableCell>{e.notes}</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {expenses.map(e => (
-              <TableRow>
-                <TableCell>{e.id}</TableCell>
-                <TableCell>{e.type}</TableCell>
-                <TableCell numeric>{e.amount}</TableCell>
-                <TableCell className='ExpensesSection__table__completed'>{e.is_completed ? <CheckMark /> : null}</TableCell>
-                <TableCell>{e.notes}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-    );
-  }
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
 
 ExpensesSection.propTypes = {
@@ -55,7 +54,7 @@ ExpensesSection.propTypes = {
     fields: PropTypes.arrayOf(PropTypes.shape({
       expenses: PropTypes.arrayOf(PropTypes.shape({
         created_at: PropTypes.string.isRequired,
-      })).isRequired,
+      })),
     })).isRequired,
   }).isRequired,
 };
