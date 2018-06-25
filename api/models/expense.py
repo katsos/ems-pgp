661e051071d23acf1a417780e61d1dd8759817b2
Nikos Katsos <nikoskatsos101@gmail.com>
2018-06-25 15:31:17 +0300

    expense.notes can be blank

--- api/models/expense.py
+++ api/models/expense.py
@@ -3,8 +3,8 @@ from django.db.models import Model, BooleanField, CharField, DateTimeField, Deci
 
 class Expense(Model):
     budget_field = ForeignKey('BudgetField', related_name='expenses', on_delete=CASCADE)
-    type = CharField(max_length=256, null=False)
-    notes = CharField(max_length=512, null=True)
+    type = CharField(max_length=256)
+    notes = CharField(max_length=512, blank=True, default='')
     amount = DecimalField(max_digits=8, decimal_places=2)
     is_completed = BooleanField()
     created_at = DateTimeField(auto_now_add=True)
