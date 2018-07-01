ad9387dba8ab62257ca6dc7dfa5e06cc4ec17337
Nikos Katsos <nikoskatsos101@gmail.com>
2018-07-01 20:33:07 +0300

    payment.notes can be blank

--- api/models/payment.py
+++ api/models/payment.py
@@ -4,7 +4,7 @@ from django.db.models import Model, CharField, DateTimeField, DecimalField, Fore
 class Payment(Model):
     student = ForeignKey('Student', related_name='payments', on_delete=CASCADE)
     amount = DecimalField(max_digits=6, decimal_places=2)
-    notes = CharField(max_length=256, null=True)
+    notes = CharField(max_length=256, blank=True, default='')
     created_at = DateTimeField(auto_now_add=True)
 
     class Meta:
